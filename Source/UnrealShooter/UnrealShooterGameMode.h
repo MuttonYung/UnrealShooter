6a40fed3711a300c6c4089e07913c223f25bc84d
MuttonYung <muttonyung@hotmail.com>
2023-09-18 17:12:10 +0800

    setup

---
+++ Source/UnrealShooter/UnrealShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealShooterGameMode();
+};
+
+
+
